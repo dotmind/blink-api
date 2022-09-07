4d957ffe350da891844152ca0395490818c6ccaf
Valentin Magry <valou32720@gmail.com>
2022-09-07 17:04:07 +0200

    [FEAT] Add delete routes & middleware

--- src/services/files/routes/index.ts
+++ src/services/files/routes/index.ts
@@ -2,8 +2,8 @@ import express, { Application } from 'express';
 
 import api from '@services/internal/infrastructure/api';
 import { checkSignature } from '@services/internal/middlewares/signature';
-import { parseHeader, registerFile, findOne, sanitizeName } from '@services/files/middlewares';
-import { upload, preview } from '@services/files/controllers';
+import { parseHeader, registerFile, findOne, sanitizeName, deleteOne } from '@services/files/middlewares';
+import { upload, preview, deleteFile } from '@services/files/controllers';
 
 const router = express.Router();
 
@@ -13,4 +13,6 @@ export default (app: Application) => {
   router.post('/upload', checkSignature, parseHeader, sanitizeName, registerFile, api.controller(upload));
 
   router.get('/preview/:id', checkSignature, findOne, api.controller(preview));
+
+  router.delete('/delete/:id', checkSignature, deleteOne, api.controller(deleteFile));
 };
