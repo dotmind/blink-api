b108e5a7a89e0aaeb32583501bfdfb9b8d17e986
Valentin Magry <valou32720@gmail.com>
2022-12-19 11:11:43 +0100

    [FEAT] Add glitchtip sentry to monitor issues

--- src/core/loaders/index.ts
+++ src/core/loaders/index.ts
@@ -1,4 +1,5 @@
 import { Express } from 'express';
+import * as Sentry from '@sentry/node';
 
 import createFolders from '@core/loaders/folder';
 import loadMiddlewares from '@core/loaders/middlewares';
@@ -7,12 +8,18 @@ import loadDatabase from '@core/loaders/database';
 import error from '@services/internal/middlewares/error';
 import notFound from '@services/internal/middlewares/notFound';
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
+
 export default async (app: Express) => {
   createFolders();
 
+  app.use(Sentry.Handlers.requestHandler());
   loadMiddlewares(app);
   await loadDatabase();
   loadProject(app);
+  app.use(Sentry.Handlers.errorHandler());
 
   app.use(error);
   app.use(notFound);
