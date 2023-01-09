765b6a02bd4f957cd8769faca98710094ae80a66
Enzo <enzo@dotmind.io>
2023-01-09 11:37:46 +0100

    [ANALYTICS] Disable in dev mode (#14)

--- src/services/analytics/middlewares/index.ts
+++ src/services/analytics/middlewares/index.ts
@@ -1,18 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 
 import { Actions, collect } from '@services/analytics/services/sheet';
+import config from '@core/config';
+
+const { isDev } = config;
 
 export const uploadAnalytics = async (req: Request, res: Response, next: NextFunction) => {
-  const { fingerprint, _path: path } = req;
-  collect(fingerprint, path, Actions.UPLOAD);
+  if (!isDev) {
+    const { fingerprint, _path: path } = req;
+    collect(fingerprint, path, Actions.UPLOAD);
+  }
 
   next();
 };
 
 export const previewAnalytics = async (req: Request, res: Response, next: NextFunction) => {
-  const { fingerprint } = req.headers;
-  const { file } = req;
-  collect(fingerprint as string, file.path, Actions.PREVIEW);
+  if (!isDev) {
+    const { fingerprint } = req.headers;
+    const { file } = req;
+    collect(fingerprint as string, file.path, Actions.PREVIEW);
+  }
 
   next();
 };
